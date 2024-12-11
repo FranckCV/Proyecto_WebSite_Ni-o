3e7486b91596363f4d9c4db312bf7590951b1155
fabss269 <fabianapm060126@gmail.com>
2024-12-11 02:07:30 -0500

    miau

---
+++ controladores/bd.py
@@ -0,0 +1,7 @@
+import pymysql
+def obtener_conexion():
+    return pymysql.connect(host='localhost',
+                                port=3307,
+                                user='root',
+                                password='',
+                                db='')
