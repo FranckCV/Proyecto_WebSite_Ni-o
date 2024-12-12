92c908b78837da5ef7e108190297abbb8619b6e2
fabss269 <fabianapm060126@gmail.com>
2024-12-12 02:54:55 -0500

    cuandoelamorllegasídeesamanera

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ def sign_up():
 def colores():
     return render_template(generalPage("colores.html"))
 
-
-
+@app.route("/pregunta")
+def pregunta():
+    return render_template(generalPage("pregunta.html"))
 
 
 
