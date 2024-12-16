ea8b64f93100f2bd1f85967fe869afc9288c14ce
fabss269 <fabianapm060126@gmail.com>
2024-12-15 23:11:32 -0500

    doufjio

--- controladores/bd.py
+++ controladores/bd.py
@@ -1,13 +1,9 @@
 import pymysql
 def obtener_conexion():
     return pymysql.connect(host='localhost',
-<<<<<<< HEAD
                                 port=3307,
                                 # port=3306,
-=======
-                                # port=3306,
-                                port=3307,
->>>>>>> 54a274cc77613a4420d2342927473f566e95cab1
+
                                 user='root',
                                 password='',
                                 db='bd_elementos')
