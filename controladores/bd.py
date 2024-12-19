392ef0c091cb13f3892fe0a895d1596bc5c554d6
ivaned_ac <edgarelcodigos@gmail.com>
2024-12-19 15:32:23 -0500

    1 2 3 PROBANDO

--- controladores/bd.py
+++ controladores/bd.py
@@ -1,7 +1,7 @@
 import pymysql
 def obtener_conexion():
     return pymysql.connect(host='localhost',
-                                port=3307,
+                                port=3306,
                                 # port=3306,
 
                                 user='root',
