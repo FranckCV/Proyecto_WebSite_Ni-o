f3aea943d79bcb8d93d4b9c7bc23c25637f318d9
fabss269 <fabianapm060126@gmail.com>
2025-01-08 23:43:18 -0500

    yafuetodo

--- controladores/bd.py
+++ controladores/bd.py
@@ -1,16 +1,7 @@
-import psycopg2
-from psycopg2 import sql
-
+import pymysql
 def obtener_conexion():
-    try:
-        conexion = psycopg2.connect(
-            host='dpg-ctveif0gph6c73eurn40-a.oregon-postgres.render.com',
-            port=5432,
-            user='root',
-            password='Vd3QqCAEeMSLJSxp2fLADRNoGYrPTqFZ',
-            dbname='bd_elementos'
-        )
-        return conexion
-    except psycopg2.Error as e:
-        print(f"Error al conectar a la base de datos: {e}")
-        return None
+    return pymysql.connect(host='localhost',    
+                                port=3307,
+                                user='root',
+                                password='',
+                                db='bd_elementos')
