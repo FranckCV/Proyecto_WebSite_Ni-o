803f3d90189d3474b2cf0fadbb088e6699ffeda7
FranckCV <francocor0105@gmail.com>
2025-01-10 16:39:25 -0500

    2025_01_10_16_39

--- static/js/toggleMenu.js
+++ static/js/toggleMenu.js
@@ -1,24 +1,8 @@
-// Ajustarr el espaciado del menú desplegable basado en el tamaño del header
-function adjustPadding() {
-    const aside = document.querySelector('aside.general_element');
-    const headerHeight = document.querySelector('header').offsetHeight;
-    if (aside) {
-        aside.style.paddingTop = `calc(15px + ${headerHeight}px)`;
-    }
-}
-
-window.addEventListener("load", adjustPadding);
-window.addEventListener("resize", adjustPadding);
-
-// Funcionalidad del menú desplegable
 const menuButton = document.querySelector("#menu_button");
 const aside = document.querySelector("aside.general_element");
 
 if (menuButton && aside) {
     menuButton.addEventListener("click", () => {
-        aside.classList.toggle("desplegar"); // Añadir o quitar la clase para mostrar/ocultar
-        adjustPadding();
+        aside.classList.toggle("desplegar");
     });
 }
-
-
